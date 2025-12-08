0d20cbac76ae50cb770cdd7c3ec62b034c07ffca
Nathan Andrew <nathannxx@proton.me>
2025-12-08 21:06:50 +0700

    tanzanite: fixup blobs and more stuffs
    - Update kernel modules from `OS2.0.206.0.VOGMIXM`
    - Update decryption blobs from `OS2.0.206.0.VOGMIXM`
    - move from Trustonic -> mitee
    - load-touch-module script by https://github.com/linastorvaldz
    - stub_scp.ko module by https://github.com/ramabondanp

--- device.mk
+++ device.mk
@@ -1,83 +1,84 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
+# Copyright (C) 2024 The Android Open Source Project
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+# Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Enable Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-ENABLE_VIRTUAL_AB := true
-AB_OTA_UPDATER := true
+# some OrangeFox-specific settings
+$(call inherit-product, $(LOCAL_PATH)/fox_tanzanite.mk)
 
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    lk \
-    odm \
-    odm_dlkm \
-    product \
-    system \
-    system_ext \
-    vbmeta_system \
-    vbmeta_vendor \
-    vendor \
-    vendor_boot \
-    vendor_dlkm
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
-    POSTINSTALL_OPTIONAL_vendor=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh
-
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
-
-# Dynamic Partitions
+# Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# API
-PRODUCT_SHIPPING_API_LEVEL := 31
-PRODUCT_TARGET_VNDK_VERSION := 31
+# V A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-# Boot control HAL
+# Bootctrl
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctl
+    bootctrl
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock
 
 # Health Hal
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
+
+# AB
+AB_OTA_UPDATER := true
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    odm_dlkm \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    vendor \
+    vendor_dlkm \
+    vendor_boot
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 34
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
 
 # Keymaster
 PRODUCT_PACKAGES += \
@@ -87,10 +88,20 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.system.keystore2
 
-# MTK plpath utils
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
+    create_pl_dev \
+    create_pl_dev.recovery
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libpuresoftkeymasterdevice \
+    android.hardware.keymaster@4.1 \
+    libkeymint
 
 # Security
 PRODUCT_PACKAGES += \
@@ -98,18 +109,8 @@ PRODUCT_PACKAGES += \
     android.hardware.security.secureclock \
     android.hardware.security.sharedsecret
 
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-# Additional configs
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1 \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2024-2025 The OrangeFox Recovery Project
+# 	Copyright (C) 2025 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -18,78 +19,21 @@
 # 	Please maintain this if you use this script or any part of it
 #
 
-#set -o xtrace
-FDEVICE="tanzanite"
-
-# Clone to fix build on minimal manifest
-git clone https://android.googlesource.com/platform/external/gflags/ -b android-12.1.0_r4 external/gflags
-
-export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v28.1.zip
+export LC_ALL="C.UTF-8"
+export FOX_MAINTAINER_PATCH_VERSION=$(date +"%Y%m%d")
 export FOX_VIRTUAL_AB_DEVICE=1
-export FOX_VANILLA_BUILD=1
 export FOX_ENABLE_APP_MANAGER=1
+export FOX_RECOVERY_VENDOR_BOOT_PARTITION="/dev/block/by-name/vendor_boot"
 export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-export FOX_USE_BASH_SHELL=1
-export FOX_ASH_IS_BASH=1
-export FOX_USE_TAR_BINARY=1
-export FOX_USE_LZ4_BINARY=1
-export FOX_USE_SED_BINARY=1
+export FOX_VENDOR_BOOT_RECOVERY=1
 export FOX_USE_XZ_UTILS=1
-export FOX_USE_ZSTD_BINARY=1
-export FOX_USE_NANO_EDITOR=1
+export FOX_USE_UPDATED_MAGISKBOOT=1
+export FOX_TARGET_DEVICES="tanzanite"
+export TARGET_DEVICE_ALT="tanzanite"
+export FOX_DISABLE_UPDATEZIP=1
 export FOX_DELETE_AROMAFM=1
-export OF_DEFAULT_KEYMASTER_VERSION=4.1
-
-# screen settings
-export OF_SCREEN_H=2460
-export OF_STATUS_H=95
-export OF_STATUS_INDENT_LEFT=48
-export OF_STATUS_INDENT_RIGHT=48
-export OF_ALLOW_DISABLE_NAVBAR=0
-export OF_CLOCK_POS=1
-
-# other stuff
-export OF_QUICK_BACKUP_LIST="/boot:/data"
-export OF_ENABLE_LPTOOLS=1
-export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-export FOX_USE_BASH_SHELL=1
-export FOX_USE_NANO_EDITOR=1
-
-# number of list options before scrollbar creation
-export OF_OPTIONS_LIST_NUM=9
-
-# ----- data format stuff -----
-# ensure that /sdcard is bind-unmounted before f2fs data repair or format
-export OF_UNBIND_SDCARD_F2FS=1
-
-# automatically wipe /metadata after data format
-export OF_WIPE_METADATA_AFTER_DATAFORMAT=1
-
-# avoid MTP issues after data format
-export OF_BIND_MOUNT_SDCARD_ON_FORMAT=1
-
-# don't spam the console with loop errors
-export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
-
-# lz4 compression
-export OF_USE_LZ4_COMPRESSION=1
-
-# build all the partition tools
-export OF_ENABLE_ALL_PARTITION_TOOLS=1
-
-# variant
-export OF_MAINTAINER="nx"
-
-# no flashlight
-export OF_FLASHLIGHT_ENABLE=0
-
-	# ccache
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
-export CCACHE_MAXSIZE="5G"
-export CCACHE_DIR=".ccache"
-
-if [ ! -d ${CCACHE_DIR} ]; then
-	mkdir $CCACHE_DIR
-fi
+export FOX_DELETE_MAGISK_ADDON=1
+export FOX_DELETE_INITD_ADDON=1
+export FOX_REMOVE_BASH=1
+export FOX_REMOVE_AAPT=1
